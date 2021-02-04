800385ba1f3ca3ce38ac0c46c4dbbf9456132273
Ambroise Vincent <ambroise.vincent@arm.com>
2021-02-04 18:29:44 +0000

    Add arm64 to CI
    Build natively on amd64 and arm64, for both cpu and cuda; bundle and
    push the resulting images.
    
    buildx is now used in the build script.
    
    The dockerfiles have been merged into one per platform. This allows for:
      * A simpler handling with buildx
      * Using the inline cache with docker build (which doesn't seem to work
        on multi-stage builds, unlike buildx)
    
    Issue-Id: SCM-2117
    Signed-off-by: Ambroise Vincent <ambroise.vincent@arm.com>
    Change-Id: I4e492f847254957f6fb6128e0196700210b9b91c

--- scripts/tvm_cli/build.sh
+++ scripts/tvm_cli/build.sh
@@ -18,6 +18,12 @@ set -e
 IMAGE_NAME="autoware/model-zoo-tvm-cli"
 TAG_NAME="local"
 FROM_ARG="ubuntu:18.04"
+TARGET_PLATFORM="arm64"
+if [[ $(uname -a) == *"x86_64"* ]]; then
+    TARGET_PLATFORM="amd64"
+fi
+CUDA_ENABLED="false"
+CI_BUILD="false"
 
 function usage() {
     echo "Usage: $0 [OPTIONS]"
@@ -27,21 +33,21 @@ function usage() {
     echo "                           Default: $IMAGE_NAME"
     echo "    -t,--tag <tag>         Tag use for the docker images."
     echo "                           Default: $TAG_NAME"
+    echo "    --platform <platform>  Set target platform. Possible values: amd64, arm64."
+    echo "                           Default: $TARGET_PLATFORM"
+    echo "    --ci                   Enable CI-relevant build options"
     echo ""
 }
 
 OPTS=$(getopt --options chi:t: \
-         --long cuda,help,image-name:,tag: \
+         --long cuda,help,image-name:,tag:,platform:,ci \
          --name "$0" -- "$@")
 eval set -- "$OPTS"
 
 while true; do
   case $1 in
     -c|--cuda)
-      FROM_ARG="nvidia/cuda-arm64:11.1-devel-ubuntu18.04"
-      if [[ $(uname -a) == *"x86_64"* ]]; then
-        FROM_ARG="nvidia/cuda:10.1-devel-ubuntu18.04"
-      fi
+      CUDA_ENABLED="true"
       shift 1
       ;;
     -h|--help)
@@ -55,6 +61,14 @@ while true; do
     -t|--tag)
       TAG_NAME="$2"
       shift 2
+      ;;
+    --platform)
+      TARGET_PLATFORM="$2"
+      shift 2
+      ;;
+    --ci)
+      CI_BUILD="true"
+      shift 1
       ;;
     --)
       if [ -n "$2" ];
@@ -71,24 +85,30 @@ while true; do
   esac
 done
 
-SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-DOCKER_FILE="Dockerfile.dependencies.arm64"
-if [[ $(uname -a) == *"x86_64"* ]]; then
-    DOCKER_FILE="Dockerfile.dependencies.amd64"
+if [ "$CUDA_ENABLED" == "true" ]; then
+  FROM_ARG="nvidia/cuda-arm64:11.1-devel-ubuntu18.04"
+  if [[ "${TARGET_PLATFORM}" == "amd64" ]]; then
+    FROM_ARG="nvidia/cuda:10.1-devel-ubuntu18.04"
+  fi
 fi
 
-BASE_IMAGE_NAME="autoware/model-zoo-tvm-cli-base:local"
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+DOCKER_FILE="Dockerfile.${TARGET_PLATFORM}"
 BUILD_CONTEXT_DIR=${SCRIPT_PATH}
 
-# Build base image with all dependencies
-docker build -f "${SCRIPT_PATH}"/"${DOCKER_FILE}" \
-             --build-arg FROM_ARG="${FROM_ARG}" \
-             -t "${BASE_IMAGE_NAME}"\
-                "${BUILD_CONTEXT_DIR}"
+EXTRA_BUILD_ARGS=""
+if [ "${CI_BUILD}" == "true" ]; then
+  EXTRA_BUILD_ARGS="--cache-from=${IMAGE_NAME}:${TAG_NAME} --cache-to=type=inline,mode=max"
+fi
 
-# Build final image with tvm_cli installed
-docker build -f "${SCRIPT_PATH}"/Dockerfile \
-             --build-arg FROM_ARG="${BASE_IMAGE_NAME}" \
-             --tag "${IMAGE_NAME}":"${TAG_NAME}" \
-                "${BUILD_CONTEXT_DIR}"
+# Build image with all dependencies and tvm_cli
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker buildx build -f "${SCRIPT_PATH}"/"${DOCKER_FILE}" \
+            --build-arg FROM_ARG="${FROM_ARG}" \
+            -t "${IMAGE_NAME}":"${TAG_NAME}" \
+            --platform ${TARGET_PLATFORM} \
+            --progress plain \
+            --load \
+            ${EXTRA_BUILD_ARGS} \
+            "${BUILD_CONTEXT_DIR}"
