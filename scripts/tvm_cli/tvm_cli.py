autowarefoundation/modelzoo

commit 09bb5722e44d27169e51285471512850a8b7763c

Add static code analysis tools (#48)
Add tools for python, cpp, and shell script code analysis.
The new checks will be run automatically on pull request.
Fixed issues with existing code.

Issue-Id: SCM-2863
Signed-off-by: Luca Foschiani <luca.foschiani@arm.com>
Change-Id: Ib759f6c076a109f1061d5370fcc1626730fb1cce

scripts/tvm_cli/tvm_cli.py
@@ -7,21 +7,22 @@
 
 import os
 import sys
+from os import path
+import importlib
 import onnx
 import yaml
-import tvm.relay.testing.tf as tf_testing
-import tvm.relay as relay
-from jinja2 import Environment, FileSystemLoader
-from tvm.contrib import cc
-from tvm import autotvm
 import tvm
-from os import path
+import tvm.relay.testing.tf as tf_testing
+import tvm.contrib.graph_runtime as runtime
+from tvm import relay
+from tvm import autotvm
+from tvm.contrib import cc
+from tvm.autotvm.tuner import XGBTuner, GATuner, RandomTuner, GridSearchTuner
+from tvm.autotvm.graph_tuner import DPTuner, PBQPTuner
+from jinja2 import Environment, FileSystemLoader
 import pytest
 import tensorflow as tf
 import numpy as np
-import tvm.contrib.graph_runtime as runtime
-from tvm.autotvm.tuner import XGBTuner, GATuner, RandomTuner, GridSearchTuner
-from tvm.autotvm.graph_tuner import DPTuner, PBQPTuner
 
 OUTPUT_NETWORK_MODULE_FILENAME = "deploy_lib.so"
 OUTPUT_NETWORK_GRAPH_FILENAME = "deploy_graph.json"
@@ -36,8 +37,8 @@ TARGETS_DEVICES = {
 }
 GPU_TARGETS = ['cuda', 'opencl', 'vulkan']
 
-# Utility function: definition.yaml file processing
 def yaml_processing(config, info):
+    '''Utility function: definition.yaml file processing'''
     with open(config, 'r') as yml_file:
         yaml_dict = yaml.safe_load(yml_file)
         # Get path of model file
@@ -67,8 +68,8 @@ def yaml_processing(config, info):
             info['output_names'].append(str(output_elem['name']))
     return info
 
-# Utility function to load the model
 def get_network(info):
+    '''Utility function to load the model'''
     if info['model'].endswith('.onnx'):
         onnx_model = onnx.load(info['model'])
         mod, params = relay.frontend.from_onnx(onnx_model, info['input_dict'])
@@ -116,9 +117,11 @@ def get_network(info):
 
     return mod, params
 
-# This function checks the command-line arguments and reads the necessary info
-# from the .yaml file, it's used when the compile option is selected
 def compilation_preprocess(args):
+    '''
+    This function checks the command-line arguments and reads the necessary info
+    from the .yaml file, it's used when the compile option is selected
+    '''
     # 'info' is the output dictionary
     info = {}
 
@@ -156,8 +159,8 @@ def compilation_preprocess(args):
 
     return info
 
-# This functions compiles the model
 def compile_model(info):
+    '''This functions compiles the model'''
     mod, params = get_network(info)
 
     # Set compilation params
@@ -210,8 +213,8 @@ def compile_model(info):
     with open(output_param_path, 'wb') as param_file:
         param_file.write(relay.save_param_dict(params))
 
-# This function generates the config .hpp file.
 def generate_config_file(info):
+    '''This function generates the config .hpp file'''
     # Setup jinja template and write the config file
     root = path.dirname(path.abspath(__file__))
     templates_dir = path.join(root, 'templates')
@@ -242,9 +245,11 @@ def generate_config_file(info):
             output_list = info['output_list']
         ))
 
-# This function checks the command-line arguments and reads the necessary info
-# from the .yaml file, it's used when the tune option is selected
 def tuning_preprocess(args):
+    '''
+    This function checks the command-line arguments and reads the necessary info
+    from the .yaml file, it's used when the tune option is selected
+    '''
     # 'info' is the output dictionary
     info = {}
 
@@ -267,23 +272,20 @@ def tuning_preprocess(args):
     # Import the AutoTVM config file
     sys.path.append(os.path.dirname(os.path.abspath(args.autotvm_config)))
     autotvm_config_file = os.path.basename(args.autotvm_config)
-    import importlib
     info['cfg'] = importlib.import_module(autotvm_config_file[:-3])
 
     return info
 
-# This function performs the tuning of a model
 def tune_model(info):
+    '''This function performs the tuning of a model'''
     def tune_kernels(
         tasks,
-        target,
-        measure_option,
-        tuner,
-        n_trial,
-        early_stopping,
-        log_filename,
-        min_exec_graph_tuner=None
+        tuning_opt
     ):
+        tuner = tuning_opt.tuner
+        n_trial = tuning_opt.n_trial
+        early_stopping = tuning_opt.early_stopping
+
         # Overwrite AutoTVM_config contents if the user provides the
         # corresponding arguments
         if info['tuner'] is not None:
@@ -297,7 +299,7 @@ def tune_model(info):
             prefix = "[Task %2d/%2d] " % (i + 1, len(tasks))
 
             # create tuner
-            if tuner == "xgb" or tuner == "xgb-rank":
+            if tuner in ('xgb', 'xgb-rank'):
                 tuner_obj = XGBTuner(tsk, loss_type="rank")
             elif tuner == "ga":
                 tuner_obj = GATuner(tsk, pop_size=100)
@@ -313,12 +315,12 @@ def tune_model(info):
             tuner_obj.tune(
                 n_trial=tsk_trial,
                 early_stopping=early_stopping,
-                measure_option=measure_option,
+                measure_option=tuning_opt.measure_option,
                 callbacks=[
                     autotvm.callback.progress_bar(tsk_trial, prefix=prefix),
                     autotvm.callback.log_to_file(path.join(
                         info['output_path'],
-                        log_filename)),
+                        tuning_opt.log_filename)),
                 ],
             )
 
@@ -417,6 +419,7 @@ if __name__ == '__main__':
     import argparse
 
     def compile():
+        '''Compiles a model using TVM'''
         parser = argparse.ArgumentParser(
             description='Compile a model using TVM',
             usage='''tvm_cli compile [<args>]''',
@@ -470,6 +473,7 @@ if __name__ == '__main__':
         return 0
 
     def tune():
+        '''Tunes a model using AutoTVM'''
         parser = argparse.ArgumentParser(
             description='Tune a model using AutoTVM',
             usage='''tvm_cli tune [<args>]''')
@@ -513,25 +517,26 @@ if __name__ == '__main__':
         tune_model(info)
 
     def test():
+        '''Launches the validation script'''
         parser = argparse.ArgumentParser(
             description='Launch the validation script',
             usage='''tvm_cli test [-h]''')
         parser.parse_args(sys.argv[2:])
         return pytest.main(['-v'])
 
-    parser = argparse.ArgumentParser(
+    main_parser = argparse.ArgumentParser(
         description='Compile model and configuration file (TVM)',
         usage='''<command> [<args>]
 Commands:
     compile    Compile a model using TVM
     tune       Tune a model using AutoTVM
     test       Launch the validation script''')
-    parser.add_argument('command', help='Subcommand to run')
-    parsed_args = parser.parse_args(sys.argv[1:2])
-    if parsed_args.command not in locals():
+    main_parser.add_argument('command', help='Subcommand to run')
+    main_parsed_args = main_parser.parse_args(sys.argv[1:2])
+    if main_parsed_args.command not in locals():
         print('Unrecognized command')
-        parser.print_help()
+        main_parser.print_help()
         exit(1)
 
     # Invoke method with same name as the argument passed
-    exit(locals()[parsed_args.command]())
+    exit(locals()[main_parsed_args.command]())

scripts/tvm_cli/validation/tvm_cli_test.py
@@ -8,24 +8,24 @@
 import os
 import shutil
 import subprocess
-import yaml
-import pytest
 from glob import glob
 from os import path
 import tempfile
 from pathlib import Path
+import yaml
+import pytest
 
 MOUNT_PATH = '/tmp'
 BACKENDS = ['llvm', 'vulkan']
 
-# Execute tvm_cli and check the return code
 def run_tvm_cli(config_path, output_folder, extra_run_args):
+    '''Execute tvm_cli and check the return code'''
     run_arg = [path.join(MOUNT_PATH, "./scripts/tvm_cli/tvm_cli.py"),
                'compile',
                '--config', config_path,
                '--output_path', output_folder]
     run_arg += extra_run_args
-    proc = subprocess.run(run_arg)
+    proc = subprocess.run(run_arg, check=True)
     assert proc.returncode == 0
 
     # Check if the files have been generated
@@ -46,8 +46,8 @@ for definition_file in definition_files:
     with open(definition_file, 'r') as yaml_file:
         yaml_dict = yaml.safe_load(yaml_file)
         if yaml_dict['enable_testing']:
-            network_name = definition_file.split(path.sep)[-3]
-            networks_to_compile[network_name] = definition_file
+            name = definition_file.split(path.sep)[-3]
+            networks_to_compile[name] = definition_file
 
 root_folder = path.join(MOUNT_PATH, 'neural_networks', os.uname().machine)
 
@@ -56,6 +56,7 @@ root_folder = path.join(MOUNT_PATH, 'neural_networks', os.uname().machine)
 @pytest.mark.parametrize('backend', BACKENDS)
 @pytest.mark.parametrize('network_name', list(networks_to_compile))
 def test_tvm_cli(backend, network_name):
+    '''Executes a test for each backend-network combination'''
     # Create a directory for every model
     output_folder = path.join(root_folder, network_name, backend)
     Path(output_folder).mkdir(parents=True, exist_ok=True)
@@ -70,6 +71,7 @@ def test_tvm_cli(backend, network_name):
                     reason='would cross-compile to itself')
 @pytest.mark.parametrize('network_name', list(networks_to_compile))
 def test_tvm_cli_cross_compile(network_name):
+    '''Executes a cross compilation test for each network'''
     # Create a directory for every model
     output_folder = tempfile.mkdtemp()
 

.github/workflows/docker-pr.yaml
@@ -10,6 +10,7 @@ jobs:
   dockerfile-lint:
 
     runs-on: ubuntu-latest
+    continue-on-error: true
 
     container:
       image: hadolint/hadolint:latest-debian
@@ -23,6 +24,62 @@ jobs:
         run: |
           hadolint scripts/tvm_cli/Dockerfile.amd64
           hadolint scripts/tvm_cli/Dockerfile.arm64
+
+  python-lint:
+
+    runs-on: ubuntu-latest
+    continue-on-error: true
+
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+
+      - name: Linting
+        run: |
+          sudo apt-get update
+          sudo apt-get install -y python3-pip
+          pip install pylint
+          find . -name "*.py" | xargs pylint \
+            --disable=import-error \
+            --disable=invalid-name \
+            --disable=missing-module-docstring \
+            --disable=redefined-builtin \
+            --disable=too-many-locals \
+            --disable=too-many-statements \
+            --disable=broad-except \
+            --disable=consider-using-sys-exit
+
+  cpp-lint:
+
+    runs-on: ubuntu-latest
+    continue-on-error: true
+
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+
+      - name: Linting
+        run: |
+          sudo apt-get update
+          sudo apt-get install -y cppcheck
+          cppcheck --enable=all --suppress=missingIncludeSystem .
+
+  shell-lint:
+
+    runs-on: ubuntu-latest
+    continue-on-error: true
+
+    container:
+      image: koalaman/shellcheck-alpine
+
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+
+      - name: Linting
+        run: |
+          find . -name "*.sh" -print0 | xargs -0 shellcheck --shell=bash
+        shell: sh
 
   setup-docker-arm64:
       runs-on: ARM64
@@ -43,7 +100,7 @@ jobs:
           run: docker system prune -a -f
 
   build-docker:
-      needs: [dockerfile-lint, setup-docker-arm64]
+      needs: [dockerfile-lint, python-lint, cpp-lint, shell-lint, setup-docker-arm64]
 
       runs-on: ${{ matrix.os }}
       strategy:
@@ -62,7 +119,7 @@ jobs:
             ./scripts/tvm_cli/build.sh -i "$IMAGE_NAME" -t "$TAG_NAME" --ci
 
   build-docker-cuda:
-      needs: [dockerfile-lint, setup-docker-arm64]
+      needs: [dockerfile-lint, python-lint, cpp-lint, shell-lint, setup-docker-arm64]
 
       runs-on: ${{ matrix.os }}
       strategy:

perception/camera_obstacle_detection/yolo_v2_tiny/tensorflow_fp32_coco/example_pipeline/main.cpp
@@ -228,8 +228,8 @@ int main(int argc, char const *argv[]) {
 
         // Transform log-space predicted coordinates to absolute space + offset
         // Transform bounding box position from offset to absolute (ratio)
-        auto x = (sigmoid(box_x) + j) / l_w;
-        auto y = (sigmoid(box_y) + i) / l_h;
+        auto x_coord = (sigmoid(box_x) + j) / l_w;
+        auto y_coord = (sigmoid(box_y) + i) / l_h;
 
         // Transform bounding box height and width from log to absolute space
         auto w = anchor_w * exp(box_w) / l_w;
@@ -267,8 +267,8 @@ int main(int argc, char const *argv[]) {
           // and back to original size
           float original_w = w * NETWORK_INPUT_WIDTH;
           float original_h = h * NETWORK_INPUT_HEIGHT;
-          float original_x = x * NETWORK_INPUT_WIDTH - original_w / 2;
-          float original_y = y * NETWORK_INPUT_HEIGHT - original_h / 2;
+          float original_x = x_coord * NETWORK_INPUT_WIDTH - original_w / 2;
+          float original_y = y_coord * NETWORK_INPUT_HEIGHT - original_h / 2;
 
           // draw rectangle on image
           cv::rectangle(

scripts/tvm_cli/build.sh
@@ -104,10 +104,15 @@ fi
 
 # Build image with all dependencies and tvm_cli
 export DOCKER_CLI_EXPERIMENTAL=enabled
+# shellcheck disable=SC2086
+# Disabling this shellcheck is required to prevent a warning about double quoting the
+# EXTRA_BUILD_ARGS variable. This variable can't be double quoted because in some cases
+# the variable itself might be empty (""), causing an error when the script tries to
+# run the "docker buildx build ..." command
 docker buildx build -f "${SCRIPT_PATH}"/"${DOCKER_FILE}" \
             --build-arg FROM_ARG="${FROM_ARG}" \
             -t "${IMAGE_NAME}":"${TAG_NAME}" \
-            --platform ${TARGET_PLATFORM} \
+            --platform "${TARGET_PLATFORM}" \
             --progress plain \
             --load \
             ${EXTRA_BUILD_ARGS} \

scripts/tvm_cli/install_tvm.sh
@@ -75,13 +75,15 @@ mkdir -p ${TVM_BUILD_DIR}
 cp ${TVM_BASE_DIR}/cmake/config.cmake ${TVM_BUILD_DIR}
 
 # turn on features
-echo "set(INSTALL_DEV TRUE)" >> ${TVM_BUILD_CONFIG}
-echo "set(USE_LLVM llvm-config-8)" >> ${TVM_BUILD_CONFIG}
-echo "set(USE_SORT ON)" >> ${TVM_BUILD_CONFIG}
-echo "set(USE_GRAPH_RUNTIME ON)" >> ${TVM_BUILD_CONFIG}
-echo "set(USE_BLAS openblas)" >> ${TVM_BUILD_CONFIG}
-echo "set(USE_OPENCL ON)" >> ${TVM_BUILD_CONFIG}
-echo "set(USE_VULKAN ON)" >> ${TVM_BUILD_CONFIG}
+{
+    echo "set(INSTALL_DEV TRUE)"
+    echo "set(USE_LLVM llvm-config-8)"
+    echo "set(USE_SORT ON)"
+    echo "set(USE_GRAPH_RUNTIME ON)"
+    echo "set(USE_BLAS openblas)"
+    echo "set(USE_OPENCL ON)"
+    echo "set(USE_VULKAN ON)"
+} >> ${TVM_BUILD_CONFIG}
 if [[ -d "/usr/local/cuda" ]]; then
     echo "set(USE_CUDA ON)" >> ${TVM_BUILD_CONFIG}
 fi

commit 82c408569834b7d113e8fe2c28e3c4a732e36999

Fix tvm_cli tune
It had been broken by 09bb572

Also fix linting errors.

Issue-Id: SCM-1397
Signed-off-by: Ambroise Vincent <ambroise.vincent@arm.com>
Change-Id: Ie59ec78eb6f17c3e3b65dce5633fd86065075cc8

scripts/tvm_cli/tvm_cli.py
@@ -39,7 +39,7 @@ GPU_TARGETS = ['cuda', 'opencl', 'vulkan']
 
 def yaml_processing(config, info):
     '''Utility function: definition.yaml file processing'''
-    with open(config, 'r') as yml_file:
+    with open(config, 'r', encoding='utf-8') as yml_file:
         yaml_dict = yaml.safe_load(yml_file)
         # Get path of model file
         info['model'] = yaml_dict['network']['filename']
@@ -206,7 +206,7 @@ def compile_model(info):
         lib.export_library(output_model_path)
 
     print('Writing graph to', output_graph_path)
-    with open(output_graph_path, 'w') as graph_file:
+    with open(output_graph_path, 'w', encoding='utf-8') as graph_file:
         graph_file.write(graph)
 
     print('Writing weights to', output_param_path)
@@ -224,7 +224,7 @@ def generate_config_file(info):
     filename = path.join(info['output_path'], OUTPUT_CONFIG_FILENAME)
 
     print('Writing pipeline configuration to', filename)
-    with open(filename, 'w') as fh:
+    with open(filename, 'w', encoding='utf-8') as fh:
         fh.write(template.render(
             namespace = info['namespace'],
             header_extension = info['header_extension'],
@@ -282,9 +282,9 @@ def tune_model(info):
         tasks,
         tuning_opt
     ):
-        tuner = tuning_opt.tuner
-        n_trial = tuning_opt.n_trial
-        early_stopping = tuning_opt.early_stopping
+        tuner = tuning_opt['tuner']
+        n_trial = tuning_opt['n_trial']
+        early_stopping = tuning_opt['early_stopping']
 
         # Overwrite AutoTVM_config contents if the user provides the
         # corresponding arguments
@@ -296,7 +296,7 @@ def tune_model(info):
             early_stopping = info['early_stopping']
 
         for i, tsk in enumerate(reversed(tasks)):
-            prefix = "[Task %2d/%2d] " % (i + 1, len(tasks))
+            prefix = f"[Task {i + 1:2d}/{len(tasks):2d}] "
 
             # create tuner
             if tuner in ('xgb', 'xgb-rank'):
@@ -315,12 +315,12 @@ def tune_model(info):
             tuner_obj.tune(
                 n_trial=tsk_trial,
                 early_stopping=early_stopping,
-                measure_option=tuning_opt.measure_option,
+                measure_option=tuning_opt['measure_option'],
                 callbacks=[
                     autotvm.callback.progress_bar(tsk_trial, prefix=prefix),
                     autotvm.callback.log_to_file(path.join(
                         info['output_path'],
-                        tuning_opt.log_filename)),
+                        tuning_opt['log_filename'])),
                 ],
             )
 
@@ -360,7 +360,7 @@ def tune_model(info):
 
     # run tuning tasks
     print("Tuning...")
-    tune_kernels(tasks, **tuning_opt)
+    tune_kernels(tasks, tuning_opt)
     if info['target'].startswith('llvm'):
         opt_sch_file = tuning_opt['log_filename'][:-4] + '_graph_opt.log'
         tune_graph(
@@ -412,8 +412,8 @@ def tune_model(info):
                                                   number=10,
                                                   repeat=60)
             prof_res = np.array(ftimer().results) * 1000
-            print("Mean inference time (std dev): %.2f ms (%.2f ms)"
-                  % (np.mean(prof_res), np.std(prof_res)))
+            print(f"Mean inference time (std dev): "
+                  f"{np.mean(prof_res):.2f} ms ({np.std(prof_res):.2f} ms)")
 
 if __name__ == '__main__':
     import argparse

scripts/tvm_cli/validation/tvm_cli_test.py
@@ -43,7 +43,7 @@ definition_files = glob(MOUNT_PATH + '/**/definition.yaml', recursive=True)
 # the files which have enable_testing: true
 networks_to_compile = {}
 for definition_file in definition_files:
-    with open(definition_file, 'r') as yaml_file:
+    with open(definition_file, 'r', encoding='utf-8') as yaml_file:
         yaml_dict = yaml.safe_load(yaml_file)
         if yaml_dict['enable_testing']:
             name = definition_file.split(path.sep)[-3]